8504359e4e078803596f8bf780c81e38966e8c44
Judy McGoldrick <jjeanius@verizon.net>
2019-01-10 20:00:10 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
 
-  validates :name, presence: true on: :create
+  validates :name, presence: true, on: :create
   validates :email, presence: true, uniqueness: true
 end
